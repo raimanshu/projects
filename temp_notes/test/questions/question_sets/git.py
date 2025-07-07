eb1edc114de93d57fb8a93273ee23b310f3c65ef
Himanshu Rai <himanshu.rai@mcubefinancial.com>
2025-07-07 15:01:54 +0530

    test

---
+++ temp_notes/test/questions/question_sets/git.py
@@ -0,0 +1,75 @@
+
+
+https://www.geeksforgeeks.org/git/git-interview-questions-and-answers/
+
+
+What is Git?
+What is a repository in Git?
+What is difference between Git and GitHub?
+What is origin in Git?
+What is the purpose of the .gitignore file?
+What is a version control system (VCS)?
+What is the git push command?
+What is the git pull command?
+What does git clone do?
+What are the advantages of using GIT?
+What is the difference between git init and git clone?
+What is git add?
+What is git status?
+What is a commit in Git?
+What is the purpose of the git clean command?
+What is a 'conflict' in git?
+What is the meaning of 'Index' in GIT?
+How do you change the last commit in git?
+What is `git checkout`?
+How do you switch branches in Git?
+Name some popular Git hosting services?
+What are the different types of Git repositories?
+How does Git handle file deletion?
+How can you create an alias in Git?
+How do you rename a branch in Git?
+What is the difference between git fetch and git pull?
+Explain Git rebase and when do you use it?
+How will you create a git repository?
+What differentiates between the commands git remote and git clone?
+What are the benefits of using a pull request in a project?
+What is a Git bundle?
+What are the advantages of Git over SVN?
+What is git stash?
+How do you revert a commit that has already been pushed and made public?
+Explain the difference between reverting and resetting?
+What is the difference between git reflog and log?
+What is the HEAD in Git?
+What is the purpose of 'git tag -a'?
+What is the difference between 'HEAD', 'working tree' and 'index' in Git?
+How to resolve a conflict in Git?
+Explain the difference between 'git merge' and 'git rebase' and when you would use each?
+What language is used in GIT?
+How do you add a file to the staging area?
+What is 'git diff?
+What is a Git commit hash?
+What is a detached HEAD state?
+How can you delete a remote Git branch?
+What is the purpose of git cherry-pick?
+What does git ls-files do?
+How do you fetch all remote branches?
+What is the Git object model?
+Explain 'git rebase' and when you would use each?
+What is a git hook and how might you use it?
+Explain the difference between git reset, git revert, and git checkout?
+How do you handle large files with Git?
+What is ‘bare repository’ in Git?
+What is branching in Git?
+What is a Git submodule?
+How can you undo a commit that hasn’t been pushed to the remote repository?
+How do you squash multiple commits into one using Git rebase?
+How do you reset a commit to a previous commit without losing changes in the working directory?
+What is the difference between git reset --hard and git clean -fd?
+How do you apply a patch from a remote Git repository?
+How do you configure a Git repository to use a specific user for a particular project?
+What is a Git reflog, and how is it useful?
+How do you manage multiple remotes in a Git repository?
+How do you configure and use Git hooks for pre-commit checks?
+How do you manage large binary files in Git without using Git LFS?
+How do you fetch changes from multiple remotes in Git?
+How do you perform a Git bisect to find the commit that introduced a bug?
---
+++ temp_notes/test/questions/question_sets/nodejs.py
@@ -0,0 +1,4 @@
+
+https://www.simplilearn.com/tutorials/nodejs-tutorial/nodejs-interview-questions
+https://www.turing.com/interview-questions/node-js
+https://www.geeksforgeeks.org/node-js/node-interview-questions-and-answers/
